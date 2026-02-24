8e144d76ea36b301714937a54507dcd9d2efc50d
sudharsan <sudharsanlrmss1998@gmail.com>
2026-02-24 19:26:59 +0530

    fixed logo path

--- src/ReusableComponents/Navbar.jsx
+++ src/ReusableComponents/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import "../ReusableComponents/Navbar.css"
 import CartContext from "../Context/CartContext";
 import { useContext } from "react";
+import logo from "../assets/logo-ecommx.png"
 
 
 const Navbar = () => {
@@ -11,7 +12,7 @@ const Navbar = () => {
 
     return(
             <nav className="navStyle">
-            <Link to="/" className="logoStyle"><img src="/src/assets/logo-ecommx.png" alt="Logo" style={{width: "100px", height: "50px"}}/></Link>
+            <Link to="/" className="logoStyle"><img src={logo} alt="Logo" style={{width: "100px", height: "50px"}}/></Link>
             <Link to="/Cart" className="cartStyle">Cart🛒<sup>{totalCartCount}</sup></Link>
             </nav>
     );
